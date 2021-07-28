7294e41e36bb0dbae14e193c885f93a4739fdf14
tomaz-suller <tomaz.suller@usp.br>
2021-07-28 09:27:45 -0300

    Add template files

---
+++ simulation/Manifest.py
@@ -0,0 +1,30 @@
+action = "simulation"
+sim_tool = "ghdl"
+top_module = "" + "_tb"
+
+sim_cmd = "ghdl -r " + top_module
+use_large_simulation = True
+open_gtkwave = True
+use_gtkwave_config = True
+config_file = None
+
+if use_large_simulation:
+    sim_cmd += " --max-stack-alloc=0"
+
+if open_gtkwave:
+    sim_cmd += " --wave=wave.ghw\n\t\tgtkwave wave.ghw"
+    if use_gtkwave_config:
+        if config_file is None:
+            sim_cmd += " config.gtkw"
+        else:
+            sim_cmd += " "+config_file
+
+sim_post_cmd = (
+    sim_cmd
+)
+
+modules = {
+    "local" : [
+        "../testbenches/",
+    ],
+}
